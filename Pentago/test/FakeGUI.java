53e776f9c2c29ed4566d15e6f74e9fceb2dc7479
Igitog <44604730+Igitog@users.noreply.github.com>
2020-11-04 19:39:42 +0100

    Add files via upload

---
+++ Pentago/test/FakeGUI.java
@@ -0,0 +1,21 @@
+
+import logic.GUIConnector;
+import logic.PlayerEnum;
+
+/**
+ *
+ * @author moritz
+ */
+public class FakeGUI implements GUIConnector {
+
+    @Override
+    public void displayStone(int[] field, PlayerEnum player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void writeLog(String log) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
